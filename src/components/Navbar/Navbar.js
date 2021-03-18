dab856d04d1ad9e92c799831b8c597c8a9de7d96
Gabriele Falloon <ggfalloon@gmail.com>
2021-03-18 13:46:25 -0500

    updated hrefs to link tags and directed to routes

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,32 +5,32 @@ import "./Navbar.css";
 
 function Navbar(props) {
     return (
-        <BrowserRouter></BrowserRouter>
+
         <div>
             <nav className="navbar fixed-top navbar-expand-md navbar-dark">
-        <div className="container">
-            <a className="navbar-brand left-nav" href="#page_top">Gabriele Falloon</a>
+                <div className="container">
+                    <Link className="navbar-brand left-nav" to="/">Gabriele Falloon</Link>
 
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
 
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav ml-auto">
-                    <li className="nav-item active">
-                        <a className="nav-link" href="#about_me">About<span className="sr-only">(current)</span></a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#portfolio">Portfolio</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#connect">Connect</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav ml-auto">
+                            <li className="nav-item active">
+                                <Link className="nav-link" to="/about">About<span className="sr-only">(current)</span></Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/portfolio">Portfolio</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/connect">Connect</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
         </div>
     )
 }
