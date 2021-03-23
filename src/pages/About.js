8116f17a0a17e27ac57204ca6c291f27624e2da3
Gabriele Falloon <ggfalloon@gmail.com>
2021-03-23 14:23:51 -0500

    about me section added

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,13 +1,42 @@
 import React from "react";
 
-
-function Projects(props) {
+function About() {
     return (
-        <div>
+        <div className="container">
+            <div className="row col-12 mx-auto">
+                <div className="col-md-6 mx-auto">
 
-            <h1>Projects</h1>
+                    <h2>About Me</h2>
+                    <br />
+                    <div className="page-header">
+
+                        <section className="details text-justify">
+                            <p>Coding is awesome! As a web developer I'm able
+                            to creatively
+                            enhance the
+                            web, improving the user experience and optimizing web performance.
+                            A great business sense and an eye for troubleshooting are
+                            just a couple of the strengths that I bring to
+                            the
+                            table. I
+                            find it very
+                            rewarding to take part in building, refactoring and maintaining something so vital to
+                            connecting our world.
+                        </p>
+                            <p>I'm looking forward to contributing my thirst for knowledge and growing skill set to a
+                            company that is
+                            pushing
+                            the
+                            boundaries of technology and utilizing the incredible possiblities of coding. When I'm
+                            not writing code, I'm
+                            cooking and staying fit. I also love to travel - I'm always ready for an
+                            adventure.</p>
+                        </section>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
 
-export default Projects;
+export default About;
