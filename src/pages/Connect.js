9576cb7910dc48b4b58fef34d50401479b7baf12
Gabriele Falloon <ggfalloon@gmail.com>
2021-03-23 14:24:42 -0500

    added Connect component

--- src/pages/Connect.js
+++ src/pages/Connect.js
@@ -1,13 +1,42 @@
 import React from "react";
 
 
-function Projects(props) {
+function Connect(props) {
     return (
-        <div>
+        <div className="container">
+            <div className="row">
+                <div className="col-md-8 mx-auto">
+                    <div className="card card-body card-padding">
+                        <h2 className="card-title h2card">Connect With Me</h2>
+                        <br />
+                        <hr className="connect-rule" />
+                        <div className="row mx-auto">
+                            <a href="mailto:ggfalloon@gmail.com"><span>
+                                <i className="fas fa-envelope link-connect grey-tooltip" data-toggle="tooltip"
+                                    data-placement="top" title="Email"></i></span>
+                            </a>
 
-            <h1>Projects</h1>
+                            <a href="https://github.com/ggfalloon" target="_blank" rel="noreferrer"><span><i
+                                className="fab fa-github link-connect grey-tooltip" data-toggle="tooltip"
+                                data-placement="top" title="Github"></i></span>
+                            </a>
+
+                            <a href="https://www.linkedin.com/in/gabriele-falloon/" target="_blank" rel="noreferrer"><span><i
+                                className="fab fa-linkedin link-connect grey-tooltip" data-toggle="tooltip"
+                                data-placement="top" title="LinkedIn"></i></span>
+                            </a>
+
+                            <a href="./G.FalloonResume.pdf"><span><i className="far fa-file link-connect grey-tooltip"
+                                data-toggle="tooltip" data-placement="top" title="Resume"></i></span>
+                            </a>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
+
     )
 }
 
-export default Projects;
+export default Connect;
