66372315592a275282f09f30f73bf27bcbdbf1ba
Gabriele Falloon <ggfalloon@gmail.com>
2021-07-27 12:34:25 -0500

    updated padding

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,7 +4,7 @@ function About() {
     return (
         <div className="pt-5" id="about">
 
-            <div className="row mx-auto p-5 about-cont">
+            <div className="row mx-auto p-3 about-cont">
                 <div className="col-md-5 mx-auto">
 
                     <h2>About Me</h2>
